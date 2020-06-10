5606edc4f3bd20a4f768f37ad3e315b69b873c63
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-06-10 16:53:20 +1000

    Initial commit

---
+++ 12889097_Hello2/USERID_Hello2/Hello2/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * This is a simple program for the first lab, to familiarise students
+ * with the lab and with plate.
+ *
+ * @author Raymond Lister
+ * @version March 2015
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        // DO NOT CHANGE THIS LINE OR ANYTHING ABOVE THIS LINE
+
+        System.out.println("Hello, PLATE!");
+
+        // DO NOT CHANGE THIS LINE OR ANYTHING BELOW THIS LINE
+    }
+}
